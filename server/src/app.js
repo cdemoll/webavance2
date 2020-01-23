3fc8abe3af560ab9afdda3d070a36df9988064f5
Ephemeral25 <clemdem61@gmail.com>
2020-01-23 21:25:43 +0100

    Added basic API functionnality

--- server/src/app.js
+++ server/src/app.js
@@ -9,9 +9,14 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+// app.get('/status', (req, res) => {
+//    res.send({
+//       message: 'Hello world'
+//    })
+// })
+app.post('/register', (req, res) => {
    res.send({
-      message: 'Hello world'
+      message: `Hello ${req.body.email} ! User successfully registered! !`
    })
 })
 
